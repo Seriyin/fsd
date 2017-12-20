5c7d2e72bacce7f1eeb6fd84b7300e9a4494f68a
Seriyin <redan.blue27@gmail.com>
2017-12-20 23:12:01 +0000

    Added Payment class as a simple bean. Represents a receipt.

---
+++ library/src/main/java/bank/Payment.java
@@ -0,0 +1,58 @@
+package bank;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Payment class contains an itemized receipt.
+ *
+ * @author André Diogo
+ * @author Diogo Pimenta
+ * @version 1.0, 20-12-2017
+ */
+public class Payment {
+    private List<Item> items;
+    private double charge;
+
+    public Payment(List<Item> items) {
+        this.items = items;
+        this.charge = items.stream().mapToDouble(Item::getCharge).sum();
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public double getCharge() {
+        return charge;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Double.compare(payment.getCharge(), getCharge()) == 0 &&
+                Objects.equals(getItems(), payment.getItems());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getItems(), getCharge());
+    }
+
+    @Override
+    public String toString() {
+        String ln = System.lineSeparator();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Payment {")
+          .append(ln);
+        items.forEach(i -> sb.append("\t").append(i).append(ln));
+        sb.append("Total Charge = ")
+          .append(charge)
+          .append(ln)
+          .append("}")
+          .append(ln);
+        return sb.toString();
+    }
+}
