3e5841e3cb8640f8711b9b5398f5cf04a3a5ca51
Seriyin <redan.blue27@gmail.com>
2017-12-20 23:59:41 +0000

    Refactored Cart class following previous spec.

--- library/src/main/java/library/Cart.java
+++ library/src/main/java/library/Cart.java
@@ -7,11 +7,42 @@ package library;
  * These include buying all the cart items and adding or removing from the cart.
  * @author André Diogo
  * @author Diogo Pimenta
- * @version 1.0, 20-12-2017
+ * @version 1.1, 20-12-2017
  */
 public interface Cart {
 
+
+    /**
+     * Adds a book to the cart.
+     * @param b book to add.
+     * @return whether it succeeded.
+     */
     boolean add(Book b);
+
+    /**
+     * Adds a book to the cart.
+     * @param isbn ISBN of the book to add.
+     * @return whether it succeeded.
+     */
+    boolean add(long isbn);
+
+    /**
+     * Removes a book from the cart.
+     * @param b the book to remove.
+     * @return whether it succeeded.
+     */
     boolean remove(Book b);
+
+    /**
+     * Removes a book from the cart.
+     * @param isbn ISBN of the book to remove.
+     * @return whether it succeeded.
+     */
+    boolean remove(long isbn);
+
+    /**
+     * Finalizes shopping by buying all books in the cart.
+     * @return whether it succeeded.
+     */
     boolean buy();
 }
