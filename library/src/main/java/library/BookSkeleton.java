ba59abc18318f44340369509ce8f1460487e2475
Seriyin <redan.blue27@gmail.com>
2017-12-20 23:59:00 +0000

    Refactored Book class into a Book Skeleton class.

--- library/src/main/java/library/BookSkeleton.java
+++ library/src/main/java/library/BookSkeleton.java
@@ -1,11 +1,89 @@
 package library;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
- * Description
- *
- * @author André Diogo
+ * BookSkeleton class contains a book's metadata.
+ * @author Andre Diogo
  * @author Diogo Pimenta
- * @version 1.0, 20-12-2017
+ * @version 1.0
  */
-public class BookSkeleton {
+public class BookSkeleton implements Book {
+
+    private long isbn;
+    private String title;
+    private List<String> authors;
+    private double price;
+
+    public BookSkeleton(long isbn, String title, List<String> authors, double price) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authors = authors;
+        this.price = price;
+    }
+
+    @Override
+    public long getISBN() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> author) {
+        this.authors = author;
+    }
+
+    @Override
+    public BookSkeleton clone() {
+        return new BookSkeleton(this.isbn,this.title,this.authors,this.price);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Book{");
+        sb.append("isbn=").append(isbn);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", author='").append(authors).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookSkeleton that = (BookSkeleton) o;
+        return isbn == that.isbn &&
+                Double.compare(that.getPrice(), getPrice()) == 0 &&
+                Objects.equals(getTitle(), that.getTitle()) &&
+                Objects.equals(getAuthors(), that.getAuthors());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(isbn, getTitle(), getAuthors(), getPrice());
+    }
 }
