8098d8c8ae9c887b374e3c5335ca96d32655edf4
Seriyin <redan.blue27@gmail.com>
2017-12-20 23:10:23 +0000

    Filled out first-draft bank Interface.

---
+++ library/src/main/java/bank/Bank.java
@@ -0,0 +1,29 @@
+package bank;
+
+import java.util.List;
+
+/**
+ * Bank describes operations allowed over payments.
+ * <p>
+ * Allows payment registration and consultation.
+ * @author André Diogo
+ * @author Diogo Pimenta
+ * @version 1.0, 20-12-2017
+ */
+public interface Bank {
+
+    /**
+     * Register a payment.
+     * @param cid The client identifier.
+     * @param p A payment that contains the items involved as well has the charge.
+     * @return whether the registration succeeded.
+     */
+    boolean register(long cid, Payment p);
+
+    /**
+     * Consult all payments to date.
+     * @param cid The client identifier.
+     * @return a list with all the payments to date.
+     */
+    List<Payment> consult(long cid);
+}
