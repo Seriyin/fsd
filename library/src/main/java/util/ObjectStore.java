d4f7c9810c08a773e6c0ce82a588879987b17cd0
Seriyin <redan.blue27@gmail.com>
2017-12-21 22:26:50 +0000

    ObjectStore's interface reflects unique-name semantics.

--- library/src/main/java/util/ObjectStore.java
+++ library/src/main/java/util/ObjectStore.java
@@ -7,13 +7,36 @@ import java.util.Optional;
  * for project-wide consultation.
  * @author Andre Diogo
  * @author Diogo Pimenta
- * @version 1.0
+ * @version 1.1
  * @see RemoteObj
  * @see pt.haslab.ekit.Clique
  */
 public interface ObjectStore {
 
+    /**
+     * Get an object reference by unique-name and tag.
+     * @param name An unique string identifying a set of objects.
+     * @param tag The tag of a specific object in that set.
+     * @return The reference or empty if it does not exist in the store.
+     */
     Optional<RemoteObj> getObject(String name, long tag);
+
+    /**
+     * Get an object reference by unique-name.
+     * @param name An unique string identifying a set of objects.
+     * @return The reference to a random object of that set, or empty if no reference exists.
+     */
     Optional<RemoteObj> getObject(String name);
-    boolean insertObject(RemoteObj ro);
+
+    /**
+     * Insert an object into a unique-name's set.
+     * <p>
+     * Can be used to store objects whose functionality is independant of the actual
+     * machine in which they are located and any such object is capable of serving
+     * the same kinds of requests.
+     * @param name Unique-name under which to insert the reference.
+     * @param ro Reference to insert under name.
+     * @return whether the insertion succeeded.
+     */
+    boolean insertObject(String name, RemoteObj ro);
 }
