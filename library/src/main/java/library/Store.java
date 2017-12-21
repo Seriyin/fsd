5444694ff12b0097f3fae98a0d4acd87674800f7
Seriyin <redan.blue27@gmail.com>
2017-12-21 00:00:10 +0000

    Refactored Store interface according to previous spec.

--- library/src/main/java/library/Store.java
+++ library/src/main/java/library/Store.java
@@ -16,12 +16,65 @@ import java.util.Optional;
  * @version 1.0, 20-12-2017
  */
 public interface Store {
-    Optional<List<Book>> findByTitle(String title);
-    Optional<List<Book>> findByAuthor(String author);
-    Optional<List<Book>> findByAuthor(List<String> authors);
-    Optional<List<Book>> findByISBN(long isbn);
-    Optional<List<Book>> findByTitleAndAuthor(String title, String author);
-    Optional<List<Book>> findByTitleAndISBN(String title, long isbn);
-    Optional<List<Book>> browse(int page);
-    Optional<List<Book>> getPurchased(int cid);
+    /**
+     * Search the store by title.
+     * @param title Title to search.
+     * @return List of none or more books matching the title.
+     */
+    List<Book> findByTitle(String title);
+
+    /**
+     * Search the store by author.
+     * @param author Name of the author to search.
+     * @return List of none or more books matching the author.
+     */
+    List<Book> findByAuthor(String author);
+
+    /**
+     * Search the store by authors.
+     * @param authors List of author names to search.
+     * @return List of none or more books matching the author.
+     */
+    List<Book> findByAuthor(List<String> authors);
+
+    /**
+     * Search the store by isbn.
+     * @param isbn ISBN to search.
+     * @return The book or null.
+     */
+    Optional<Book> findByISBN(long isbn);
+
+    /**
+     * Search the store by author and title.
+     * @param title Title of the book to search.
+     * @param author Name of the author to search.
+     * @return List of none or more books matching the author.
+     */
+    List<Book> findByTitleAndAuthor(String title, String author);
+
+    /**
+     * Search the store by authors and title.
+     * @param title Title of the book to search.
+     * @param author List of author names to search.
+     * @return List of none or more books matching the author.
+     */
+    List<Book> findByTitleAndAuthor(String title, List<String> author);
+
+    /**
+     * Lists a page of existing books in the store.
+     * @return List containing a page of books in the store.
+     */
+    List<Book> browse(int page);
+
+    /**
+     * Lists all existing books in the store.
+     * @return List containing all the books in the store.
+     */
+    List<Book> listBooks();
+
+    /**
+     * Lists all books purchased by a client in the store.
+     * @return List containing all the books purchased by the client in the store.
+     */
+    List<Book> getPurchased(int cid);
 }
