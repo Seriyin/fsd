9db1f09bcef3aac2ad48dc27c5c3784a0a758173
Seriyin <redan.blue27@gmail.com>
2017-12-22 23:44:53 +0000

    Stubs need only RemoteObject involved. Double-check export for null.

--- library/src/main/java/util/RemoteObjFactory.java
+++ library/src/main/java/util/RemoteObjFactory.java
@@ -4,7 +4,6 @@ import io.atomix.catalyst.transport.Address;
 import library.Book;
 import library.Cart;
 import library.Store;
-import pt.haslab.ekit.Clique;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,12 +17,11 @@ import java.util.concurrent.atomic.AtomicLong;
  *
  * @author André Diogo
  * @author Diogo Pimenta
- * @version 1.0, 21-12-2017
+ * @version 1.2, 22-12-2017
  * @see RemoteObj
  * @see DistObjManager
  */
 final class RemoteObjFactory {
-    private Clique cl;
     private Address address;
     private Map <String,Map<Long,Object>> objstr;
     private AtomicLong tag;
@@ -32,13 +30,11 @@ final class RemoteObjFactory {
      * RemoteObjFactory needs to know the address or index(for Clique) in which it is
      * operating to assign to references it generates.
      * Needs the underlying store reference to populate with exported objects.
-     * @param cl The Clique in which the DistObjManager is located.
      * @param address The network address of the process's DistObjManager.
      * @param objstr The object store of the DistObjManager.
      * @see DistObjManager
      */
-    RemoteObjFactory(Clique cl, Address address, Map<String, Map<Long, Object>> objstr) {
-        this.cl = cl;
+    RemoteObjFactory(Address address, Map<String, Map<Long, Object>> objstr) {
         this.address = address;
         this.objstr = objstr;
         this.tag = new AtomicLong(0);
@@ -57,52 +53,53 @@ final class RemoteObjFactory {
     protected Optional<Object> importRef(RemoteObj b) {
         String cls = b.getCls();
         if (cls.equals(Book.class.getName())) {
-            return Optional.of(new BookStub(cl, b, address));
+            return Optional.of(new BookStub(b));
         }
         else if(cls.equals(Cart.class.getName())) {
-            return Optional.of(new CartStub(cl, b, address));
+            return Optional.of(new CartStub(b));
         }
         else if(cls.equals(Store.class.getName())) {
-            return Optional.of(new StoreStub(cl, b, address));
+            return Optional.of(new StoreStub(b));
         }
         else if(cls.equals(ObjectStore.class.getName())) {
-            return Optional.of(new ObjectStoreStub(cl, b, address));
+            return Optional.of(new ObjectStoreStub(b));
         }
         return Optional.empty();
     }
 
     /**
      * Exports a reference from a given remote object.
-     * <p>
-     * Assumes a valid (non-null) Object as parameter.
      * @param b The object to export
-     * @return A reference or empty(in case of invalid class -> caller ensures it never happens)
+     * @return A reference or empty(in case of invalid class/null-parameter -> caller ensures it never happens)
      * @see RemoteObj
      */
     protected Optional<RemoteObj> exportRef(Object b) {
-        Map<Long,Object> mp = null;
-        String cls;
-        if (b instanceof Book) {
-            cls = Book.class.getName();
+        if(b!=null) {
+            Map<Long,Object> mp = null;
+            String cls;
+            if (b instanceof Book) {
+                cls = Book.class.getName();
+            }
+            else if (b instanceof Cart) {
+                cls = Cart.class.getName();
+            }
+            else if (b instanceof ObjectStore) {
+                cls = ObjectStore.class.getName();
+            }
+            else if (b instanceof Store) {
+                cls = Store.class.getName();
+            }
+            else {
+                return Optional.empty();
+            }
+            if(!objstr.containsKey(cls)) {
+                objstr.put(cls,new HashMap<>());
+                mp = objstr.get(cls);
+            }
+            long id = tag.getAndIncrement();
+            mp.put(id,b);
+            return Optional.of(new RemoteObj(address,id,cls));
         }
-        else if (b instanceof Cart) {
-            cls = Cart.class.getName();
-        }
-        else if (b instanceof ObjectStore) {
-            cls = ObjectStore.class.getName();
-        }
-        else if (b instanceof Store) {
-            cls = Store.class.getName();
-        }
-        else {
-            return Optional.empty();
-        }
-        if(!objstr.containsKey(cls)) {
-            objstr.put(cls,new HashMap<>());
-            mp = objstr.get(cls);
-        }
-        long id = tag.getAndIncrement();
-        mp.put(id,b);
-        return Optional.of(new RemoteObj(address,id,cls));
+        return Optional.empty();
     }
 }
