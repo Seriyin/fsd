6f69a60e8c8b8a20efb37f7e552809e67cdd429c
Seriyin <redan.blue27@gmail.com>
2017-12-23 00:33:58 +0000

    Separate boilerplate into an abstract Server class.

---
+++ library/src/main/java/bank/BankServer.java
@@ -0,0 +1,23 @@
+package bank;
+
+
+import util.Server;
+
+/**
+ * StoreServer class contains the main logic to boot up a process in a connected
+ * network.
+ * @author Andre Diogo
+ * @author Diogo Pimenta
+ * @version 1.0
+ * @see pt.haslab.ekit.Clique
+ */
+public class BankServer extends Server {
+    public static void main(String args[]) {
+
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+}
