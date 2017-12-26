e8bdddd4f8f3f317bfc0d1577db09d2c5f04a085
André Diogo <redan.blue27@gmail.com>
2017-12-26 18:51:38 +0000

    Refactored RemoteObjFactory's implementation to separate class (IOC).

---
+++ bookstore/src/util/RemoteObjFactoryImpl.java
@@ -0,0 +1,140 @@
+package util;
+
+import bank.Bank;
+import bank.BankStub;
+import io.atomix.catalyst.transport.Address;
+import io.atomix.catalyst.transport.Connection;
+import io.atomix.catalyst.transport.Transport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import store.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Class responsible for figuring out object type, generating references
+ * and inserting the objects into the exported objects store.
+ *
+ *
+ * @author André Diogo
+ * @author Diogo Pimenta
+ * @version 1.3, 26-12-2017
+ * @see RemoteObj
+ * @see DistObjManager
+ */
+final class RemoteObjFactoryImpl implements RemoteObjFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(RemoteObjFactoryImpl.class);
+    private Address address;
+    private Map <String,Map<Long,Object>> objstr;
+    private AtomicLong tag;
+    private Transport t;
+
+    /**
+     * RemoteObjFactoryImpl needs to know the address or index(for Clique) in which it is
+     * operating to assign to references it generates.
+     * Needs the underlying store reference to populate with exported objects.
+     * @param address The network address of the process's DistObjManager.
+     * @param objstr The object store of the DistObjManager.
+     * @see DistObjManager
+     */
+    RemoteObjFactoryImpl(Address address, Transport t, Map<String, Map<Long, Object>> objstr) {
+        this.address = address;
+        this.t = t;
+        this.objstr = objstr;
+        this.tag = new AtomicLong(0);
+    }
+
+    /**
+     * Imports a stub from a given remote object.
+     * <p>
+     * Stubs are infused with the calling processes network info,
+     * in order to delegate actual method invocations through RMI
+     * transparently.
+     * @param b The object reference
+     * @return A stub or empty(in case of invalid class -> should never happen)
+     * @see RemoteObj
+     */
+     public Optional<Object> importRef(RemoteObj b) {
+        String cls = b.getCls();
+        Optional result;
+        if (cls.equals(Book.class.getName())) {
+            result = Optional.of(new BookStub(b,t));
+        }
+        else if(cls.equals(Cart.class.getName())) {
+            result = Optional.of(new CartStub(b,t));
+        }
+        else if(cls.equals(Store.class.getName())) {
+            result = Optional.of(new StoreStub(b,t));
+        }
+        else if(cls.equals(ObjectStore.class.getName())) {
+            result = Optional.of(new ObjectStoreStub(b,t));
+        }
+        else if(cls.equals(Bank.class.getName())) {
+            result = Optional.of(new BankStub(b,t));
+        }
+        else if(cls.equals(TransactionsManager.class.getName())) {
+            result = Optional.of(new TransactionsManagerStub(b,t);
+        }
+        else {
+            result = Optional.empty();
+        }
+        return result;
+    }
+
+    /**
+     * Exports a reference from a given remote object.
+     * @param b The object to export
+     * @return A reference or empty(in case of invalid class/null-parameter -> caller ensures it never happens)
+     * @see RemoteObj
+     */
+    public Optional<RemoteObj> exportRef(Object b) {
+        Optional<RemoteObj> result;
+        if(b!=null) {
+            Map<Long,Object> mp;
+            String cls = null;
+            if (b instanceof Book) {
+                cls = Book.class.getName();
+            }
+            else if (b instanceof Cart) {
+                cls = Cart.class.getName();
+            }
+            else if (b instanceof ObjectStore) {
+                cls = ObjectStore.class.getName();
+            }
+            else if (b instanceof Store) {
+                cls = Store.class.getName();
+            }
+            else if (b instanceof Bank) {
+                cls = Bank.class.getName();
+            }
+            else if (b instanceof TransactionsManager) {
+                cls = TransactionsManager.class.getName();
+            }
+            else {
+                LOG.debug("Empty Ref - "+ b);
+            }
+            if(cls!=null) {
+                if(!objstr.containsKey(cls)) {
+                    objstr.put(cls,new HashMap<>());
+                    mp = objstr.get(cls);
+                }
+                long id = tag.getAndIncrement();
+                mp.put(id,b);
+                LOG.debug("Export Ref - "+cls);
+                result = Optional.of(new RemoteObj(address,id,cls));
+            }
+            else {
+                result = Optional.empty();
+            }
+        }
+        else {
+            LOG.debug("Ref null");
+            result = Optional.empty();
+        }
+        return result;
+    }
+
+}
