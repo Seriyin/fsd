205f5e7829388fa9cf597aac0b64f2e1fd48493b
André Diogo <redan.blue27@gmail.com>
2017-12-29 19:00:26 +0000

    RegisterReply returns whether the registration succeeded in the NamingService.

---
+++ bookstore/src/messaging/RegisterReply.java
@@ -0,0 +1,31 @@
+package messaging;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.CatalystSerializable;
+import io.atomix.catalyst.serializer.Serializer;
+
+/**
+ * Register reply is a simple reply to a naming service registration,
+ * with a success indication.
+ *
+ * @author André Diogo
+ * @version 1.0, 29-12-2017
+ */
+public class RegisterReply implements CatalystSerializable {
+    boolean hasSucceeded;
+
+    public boolean HasSucceeded() {
+        return hasSucceeded;
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+        buffer.writeBoolean(hasSucceeded);
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {
+        hasSucceeded = buffer.readBoolean();
+    }
+}
