e8743ff15196c7fbfbc489dbf7ebd127b792d0c9
André Diogo <redan.blue27@gmail.com>
2017-12-29 21:44:04 +0000

    Simplified RemoteObjFactory interface as an extension of Exporter and Importer.

--- bookstore/src/util/RemoteObjFactory.java
+++ bookstore/src/util/RemoteObjFactory.java
@@ -1,28 +1,13 @@
 package util;
 
-import java.util.Optional;
-
-
 /**
- * Interface that describes how to import and export RemoteObjs;
+ * Interface that describes how to import and export RemoteObjs, that
+ * factories of RemoteObj must adhere to.
+ *
+ * They must implement export and import.
  *
  * @author André Diogo
- * @version 1.0, 22-12-2017
+ * @version 1.1, 29-12-2017
  * @see RemoteObjFactoryImpl
  */
-public interface RemoteObjFactory {
-
-    /**
-     * Import a stub from a RemoteObj instance.
-     * @param ro
-     * @return A stub or nothing.
-     */
-    Optional<Object> importRef(RemoteObj ro);
-
-    /**
-     * Export a RemoteObj from an arbitrary Object.
-     * @param o
-     * @return A RemoteObj or nothing.
-     */
-    Optional<RemoteObj> exportRef(Object o);
-}
+public interface RemoteObjFactory extends Importer,Exporter {}
