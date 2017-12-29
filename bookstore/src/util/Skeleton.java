24f10b45619e4fe7b28e6ce7b36f2a7c46d7ad77
André Diogo <redan.blue27@gmail.com>
2017-12-29 23:35:11 +0000

    Skeleton manages the RemoteObj reference for the underlying class.

---
+++ bookstore/src/util/Skeleton.java
@@ -0,0 +1,32 @@
+package util;
+
+/**
+ * Skeleton contains {@link RemoteObj} management methods.
+ */
+public abstract class Skeleton {
+    private RemoteObj ro = null;
+
+    /**
+     * Returns the underlying skeleton's object reference.
+     * @return null or RemoteObj of this skeleton.
+     */
+    protected RemoteObj getRef() {
+        return ro;
+    }
+
+    /**
+     * Set the underlying skeleton's remote reference.
+     * Pair with an exportRef call in a {@link Server}.
+     * @param ro The RemoteObj to set on this skeleton.
+     */
+    protected void setRef(RemoteObj ro) {
+        this.ro = ro;
+    }
+
+    /**
+     * @return if the object has already been exported.
+     */
+    protected boolean isExported() {
+        return ro!=null;
+    }
+}
