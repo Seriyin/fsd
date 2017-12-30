879639f6bd8e667e6ce253d5e86deb74325a3a16
André Diogo <redan.blue27@gmail.com>
2017-12-30 02:51:20 +0000

    Clean up javadoc.

--- bookstore/src/util/TransactionsManager.java
+++ bookstore/src/util/TransactionsManager.java
@@ -3,10 +3,6 @@ package util;
 /**
  * TransactionsManager will be responsible for ensuring book buys are atomic.
  * Should be more general as to ensure arbitrary operations occur atomically.
- *
- * @author André Diogo
- * @author Diogo Pimenta
- * @version 1.0, 20-12-2017
  */
 public interface TransactionsManager {
 }
