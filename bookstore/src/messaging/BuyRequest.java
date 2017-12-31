113e9cd2bc1d84183c95aefc6153a320dff2e22b
André Diogo <redan.blue27@gmail.com>
2017-12-31 23:37:06 +0000

    Buy Request just serializes to signal corresponding cart.

--- bookstore/src/messaging/BuyRequest.java
+++ bookstore/src/messaging/BuyRequest.java
@@ -1,7 +1,26 @@
 package messaging;
 
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.Serializer;
+import util.RemoteObj;
+
 /**
- *
+ * BuyRequest is just a distinguishable empty request to a cart.
  */
-public class BuyRequest {
+public class BuyRequest extends ObjRequest {
+
+    protected BuyRequest(RemoteObj ro) {
+        super(ro);
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+        serializer.writeObject(getRemoteObj());
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {
+        setRemoteObj(serializer.readObject(buffer));
+    }
 }
