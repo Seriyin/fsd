a4c83fdc7eab29f1536729cfb9a948050b0916ce
André Diogo <redan.blue27@gmail.com>
2017-12-31 23:35:35 +0000

    Add the SReply class that contains a simple success boolean.

---
+++ bookstore/src/messaging/SReply.java
@@ -0,0 +1,31 @@
+package messaging;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.CatalystSerializable;
+import io.atomix.catalyst.serializer.Serializer;
+
+/**
+ * Reply which contains a boolean indicating operation success.
+ */
+public abstract class SReply implements CatalystSerializable {
+    private boolean hasSucceeded;
+
+    SReply(boolean hasSucceeded){
+        this.hasSucceeded = hasSucceeded;
+    }
+
+    public boolean hasSucceeded() {
+        return hasSucceeded;
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+        buffer.writeBoolean(hasSucceeded);
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {
+        hasSucceeded = buffer.readBoolean();
+    }
+}
