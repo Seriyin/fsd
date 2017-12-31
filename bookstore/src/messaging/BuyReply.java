3a1edfd686ed70067fc10b428fdb634dfe6495d9
André Diogo <redan.blue27@gmail.com>
2017-12-31 23:36:29 +0000

    Buy Reply just indicates success and serializes through super.

--- bookstore/src/messaging/BuyReply.java
+++ bookstore/src/messaging/BuyReply.java
@@ -1,7 +1,24 @@
 package messaging;
 
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.Serializer;
+
 /**
- * Description.
+ * BuyReply holds the purchase transaction result.
  */
-public class BuyReply {
+public class BuyReply extends SReply {
+    public BuyReply(boolean hasSucceeded) {
+        super(hasSucceeded);
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+        super.writeObject(buffer,serializer);
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {
+        super.readObject(buffer,serializer);
+    }
 }
