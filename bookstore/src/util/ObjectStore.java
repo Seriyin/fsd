a74af684c0c0eeffa254376c8f323b37046c5b44
André Diogo <redan.blue27@gmail.com>
2017-12-31 23:43:10 +0000

    ObjectStore refactored as arbitrary named + tagged container.

--- bookstore/src/util/ObjectStore.java
+++ bookstore/src/util/ObjectStore.java
@@ -4,11 +4,10 @@ import java.util.Optional;
 
 /**
  * ObjectStore is an interface that describes how to store and
- * recover RemoteObj instances for project-wide consultation.
- * @see RemoteObj
- * @see pt.haslab.ekit.Clique
+ * recover Objects instances into a structured container.
  */
-public interface ObjectStore {
+public interface ObjectStore<T> {
+
 
     /**
      * Get an object reference by unique-name and tag.
@@ -16,25 +15,18 @@ public interface ObjectStore {
      * @param tag The tag of a specific object in that set.
      * @return The reference or empty if it does not exist in the store.
      */
-    Optional<RemoteObj> getObject(String name, long tag);
-
-    /**
-     * Get an object reference by unique-name.
-     * @param name An unique string identifying a set of objects.
-     * @return The reference to a random object of that set,
-     *          or empty if no reference exists.
-     */
-    Optional<RemoteObj> getObject(String name);
+    Optional<T> getObject(String name, long tag);
 
     /**
      * Insert an object into a unique-name's set.
      * <p>
-     * Can be used to store objects whose functionality is independant of the actual
-     * machine in which they are located and any such object is capable of serving
-     * the same kinds of requests.
+     * Can be used to store objects with both addressing and uniqueness.
+     * <p>
      * @param name Unique-name under which to insert the reference.
+     * @param tag Unique-number under which to insert the reference.
      * @param ro Reference to insert under name.
      * @return whether the insertion succeeded.
      */
-    boolean insertObject(String name, RemoteObj ro);
+    boolean insertObject(String name, long tag, T ro);
+
 }
