149e5eab66c31019992f5130c378130b494c469f
André Diogo <redan.blue27@gmail.com>
2017-12-31 23:44:00 +0000

    PurchaseReply is an empty SReply.

---
+++ bookstore/src/messaging/PurchaseReply.java
@@ -0,0 +1,25 @@
+package messaging;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.Serializer;
+
+/**
+ * Purchase Reply just indicates operation success.
+ */
+public class PurchaseReply extends SReply {
+
+    public PurchaseReply(boolean hasSucceeded) {
+        super(hasSucceeded);
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {
+        super.readObject(buffer, serializer);
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+        super.writeObject(buffer, serializer);
+    }
+}
