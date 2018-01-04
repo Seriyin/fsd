3a6442851db7243cdb8472638dab266a5764e841
Seriyin <redan.blue27@gmail.com>
2018-01-04 17:06:29 +0000

    Transaction manager interface added.

--- bookstore/src/util/TransactionsManager.java
+++ bookstore/src/util/TransactionsManager.java
@@ -1,8 +1,53 @@
 package util;
 
+import io.atomix.catalyst.serializer.CatalystSerializable;
+
 /**
  * TransactionsManager will be responsible for ensuring book buys are atomic.
+ * <p>
  * Should be more general as to ensure arbitrary operations occur atomically.
  */
 public interface TransactionsManager {
+
+    /**
+     * Create transaction-context and send it to the client.
+     * <p>
+     * Works synchronously for obvious reasons through use of
+     * {@link io.atomix.catalyst.transport.Connection#sendAndReceive(Object)}.
+     * @return a transaction id or -1 if error.
+     */
+    long begin();
+
+    /**
+     * Implicit process registration into this transaction-context.
+     * @param xid The transaction id.
+     * @param ro reference of resource to contact.
+     * @param logged object to log.
+     */
+    <T extends CatalystSerializable> void heartbeat(long xid, RemoteObj ro, T logged);
+
+    /**
+     * Client request to prepare resources for commit.
+     * <p>
+     * Initializes first stage of 2PC.
+     * @param xid The transaction id.
+     */
+    void prepare(long xid);
+
+    /**
+     * Handle prepared response from resource.
+     * @param xid The transaction id.
+     * @param ro remote reference of resource that's prepared.
+     */
+    void prepared(long xid, RemoteObj ro);
+
+    /**
+     *  Handle abort request from a resource.
+     *  <p>
+     *  Will result in abort request to all resources
+     *  and inform client.
+     *  @param xid The transaction id.
+     */
+    void abort(long xid);
+
 }
