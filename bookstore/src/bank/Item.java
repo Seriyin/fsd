ae69d1b3030573bd7999ed3d68d3402c26c479a0
Seriyin <redan.blue27@gmail.com>
2018-01-04 16:34:13 +0000

    Item extended to total charge and quantity.

--- bookstore/src/bank/Item.java
+++ bookstore/src/bank/Item.java
@@ -8,16 +8,20 @@ import io.atomix.catalyst.serializer.Serializer;
 import java.util.Objects;
 
 /**
- * Item is a simple immutable object containing an item description
- * and a value charged for that item.
+ * Item is a simple immutable object containing an item description,
+ * quantity and value charged for that item.
  */
 public class Item implements CatalystSerializable {
     private String item;
+    private int quantity;
     private double charge;
+    private double totalCharge;
 
-    public Item(String item, double charge) {
+    public Item(String item, double charge, int quantity) {
         this.item = item;
         this.charge = charge;
+        this.quantity = quantity;
+        totalCharge = charge * quantity;
     }
 
     public String getItem() {
@@ -28,19 +32,38 @@ public class Item implements CatalystSerializable {
         return charge;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item1 = (Item) o;
-        return Double.compare(item1.getCharge(), getCharge()) == 0 &&
-                Objects.equals(getItem(), item1.getItem());
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public double getTotalCharge()
+    {
+        return totalCharge;
     }
 
     @Override
-    public int hashCode() {
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        Item item1 = (Item) o;
+        return getQuantity() == item1.getQuantity() &&
+               Double.compare(item1.getCharge(), getCharge()) == 0 &&
+               Double.compare(item1.getTotalCharge(), getTotalCharge()) == 0 &&
+               Objects.equals(getItem(), item1.getItem());
+    }
 
-        return Objects.hash(getItem(), getCharge());
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(getItem(), getQuantity(), getCharge(), getTotalCharge());
     }
 
     @Override
@@ -48,8 +71,12 @@ public class Item implements CatalystSerializable {
         StringBuilder sb = new StringBuilder();
         sb.append("Item { item='")
           .append(item)
-          .append("', charge=")
+          .append("', charge per item=")
           .append(charge)
+          .append(", quantity=")
+          .append(quantity)
+          .append(", total charge=")
+          .append(totalCharge)
           .append(" }");
         return sb.toString();
     }
@@ -58,11 +85,15 @@ public class Item implements CatalystSerializable {
     public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
         buffer.writeString(item);
         buffer.writeDouble(charge);
+        buffer.writeInt(quantity);
+        buffer.writeDouble(totalCharge);
     }
 
     @Override
     public void readObject(BufferInput<?> buffer, Serializer serializer) {
         item = buffer.readString();
         charge = buffer.readDouble();
+        quantity = buffer.readInt();
+        totalCharge = buffer.readDouble();
     }
 }
