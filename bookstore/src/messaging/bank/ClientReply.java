42fb33fa248a9cfbceacf6535077e30b23ab49b7
Seriyin <redan.blue27@gmail.com>
2018-01-04 16:36:01 +0000

    Added client reply

---
+++ bookstore/src/messaging/bank/ClientReply.java
@@ -0,0 +1,29 @@
+package messaging.bank;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.Serializer;
+import messaging.util.SReply;
+
+/**
+ * Client reply is yet another success of fail reply.
+ */
+public class ClientReply extends SReply
+{
+    public ClientReply(boolean hasSucceeded)
+    {
+        super(hasSucceeded);
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer)
+    {
+        super.readObject(buffer, serializer);
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer)
+    {
+        super.writeObject(buffer, serializer);
+    }
+}
