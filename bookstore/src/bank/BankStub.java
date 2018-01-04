da7f8d023c09447d5c5049772e808169eacba627
Seriyin <redan.blue27@gmail.com>
2018-01-04 16:32:57 +0000

    Since no caching is present yet, synchronous request replies to preserve simple transparent interface.

--- bookstore/src/bank/BankStub.java
+++ bookstore/src/bank/BankStub.java
@@ -1,30 +1,57 @@
 package bank;
 
 import io.atomix.catalyst.transport.Connection;
-import io.atomix.catalyst.transport.Transport;
+import messaging.bank.*;
 import util.RemoteObj;
 import util.Stub;
 
 import java.util.List;
 
+
+/**
+ * #TODO meaningful caching.
+ * <p>
+ * Sends requests to the actual underlying bank.
+ * <p>
+ * Acts synchronously to keep interface standard and
+ * make distributed behaviour transparent.
+ */
 public class BankStub extends Stub implements Bank {
-    private List<Payment> paymentCache;
-
-
+    /**
+     * Constructor that takes regular stub parameters.
+     * @param b remote reference to bank resource.
+     * @param c client connection to bank resource
+     */
     public BankStub(RemoteObj b, Connection c)
     {
         super(b,c);
-        paymentCache = null;
     }
 
     @Override
-    public boolean register(long cid, Payment p) {
-        return false;
+    public boolean registerClient(String name)
+    {
+        return getConnection().<ClientRequest, ClientReply>
+                                       sendAndReceive(new ClientRequest(getRef(), name))
+                              .join()
+                              .hasSucceeded();
+    }
+
+    @Override
+    public boolean registerPayment(long cid, Payment p)
+    {
+        return getConnection()
+                       .<PurchaseRequest, PurchaseReply>
+                                sendAndReceive(new PurchaseRequest(getRef(), cid, p))
+                       .join()
+                       .hasSucceeded();
     }
 
     @Override
     public List<Payment> consult(long cid) {
-        return null;
+        return getConnection().<ConsultRequest, ConsultReply>
+                                       sendAndReceive(new ConsultRequest(getRef(), cid))
+                              .join()
+                              .getPaymentHistory();
     }
 
 }
