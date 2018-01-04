31135c24991009a0e9c6b19ae18801637e8573be
Seriyin <redan.blue27@gmail.com>
2018-01-04 16:42:01 +0000

    Add and remove request works over books and quantities.

--- bookstore/src/messaging/store/AddRequest.java
+++ bookstore/src/messaging/store/AddRequest.java
@@ -1,33 +1,46 @@
-package messaging;
+package messaging.store;
 
 import io.atomix.catalyst.buffer.BufferInput;
 import io.atomix.catalyst.buffer.BufferOutput;
 import io.atomix.catalyst.serializer.Serializer;
+import messaging.util.ObjRequest;
+import store.Book;
 import util.RemoteObj;
 
 /**
  * AddRequest signals Cart for adding book referenced by ISBN.
  */
-public class AddRequest extends ObjRequest {
-    private long isbn;
+public class AddRequest extends ObjRequest
+{
+    private Book b;
+    private int qt;
 
-    public AddRequest(RemoteObj ref, long isbn) {
+    public AddRequest(RemoteObj ref, Book b, int qt)
+    {
         super(ref);
+        this.b = b;
+        this.qt = qt;
     }
 
-    public long getIsbn() {
-        return isbn;
+    public Book getBook() {
+        return b;
+    }
+
+    public int getQuantity() {
+        return qt;
     }
 
     @Override
     public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
         super.writeObject(buffer, serializer);
-        buffer.writeLong(isbn);
+        serializer.writeObject(b,buffer);
+        buffer.writeInt(qt);
     }
 
     @Override
     public void readObject(BufferInput<?> buffer, Serializer serializer) {
         super.readObject(buffer, serializer);
-        isbn = buffer.readLong();
+        b = serializer.readObject(buffer);
+        qt = buffer.readInt();
     }
 }
--- bookstore/src/messaging/store/RemoveRequest.java
+++ bookstore/src/messaging/store/RemoveRequest.java
@@ -1,33 +1,48 @@
-package messaging;
+package messaging.store;
 
 import io.atomix.catalyst.buffer.BufferInput;
 import io.atomix.catalyst.buffer.BufferOutput;
 import io.atomix.catalyst.serializer.Serializer;
+import messaging.util.ObjRequest;
+import store.Book;
 import util.RemoteObj;
 
 /**
  * RemoveRequest signals Cart for removing book referenced by ISBN.
  */
-public class RemoveRequest extends ObjRequest {
-    private long isbn;
+public class RemoveRequest extends ObjRequest
+{
+    private Book b;
+    private int qt;
 
-    public RemoveRequest(RemoteObj ref, long isbn) {
+    public RemoveRequest(RemoteObj ref, Book b, int qt)
+    {
         super(ref);
+        this.b = b;
+        this.qt = qt;
     }
 
-    public long getIsbn() {
-        return isbn;
+    public Book getBook()
+    {
+        return b;
+    }
+
+    public int getQuantity()
+    {
+        return qt;
     }
 
     @Override
     public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
         super.writeObject(buffer, serializer);
-        buffer.writeLong(isbn);
+        serializer.writeObject(b,buffer);
+        buffer.writeInt(qt);
     }
 
     @Override
     public void readObject(BufferInput<?> buffer, Serializer serializer) {
         super.readObject(buffer, serializer);
-        isbn = buffer.readLong();
+        b = serializer.readObject(buffer);
+        qt = buffer.readInt();
     }
 }
