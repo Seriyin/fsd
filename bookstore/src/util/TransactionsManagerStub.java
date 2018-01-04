717a759f05bb528cc2454e33e1ecd2ec8b080c87
Seriyin <redan.blue27@gmail.com>
2018-01-04 17:08:06 +0000

    Stub just fires off async requests except for beginning a transaction which must return the txid.

--- bookstore/src/util/TransactionsManagerStub.java
+++ bookstore/src/util/TransactionsManagerStub.java
@@ -1,12 +1,42 @@
 package util;
 
+import io.atomix.catalyst.serializer.CatalystSerializable;
 import io.atomix.catalyst.transport.Connection;
-import io.atomix.catalyst.transport.Transport;
+import messaging.util.*;
 
 public class TransactionsManagerStub extends Stub implements TransactionsManager {
 
 
     public TransactionsManagerStub(RemoteObj b, Connection c) {
         super(b,c);
+    }
+
+    @Override
+    public long begin() {
+        return getConnection().<BeginTransactionRequest, BeginTransactionReply>sendAndReceive(new BeginTransactionRequest(getRef()))
+                              .join()
+                              .getXID();
+    }
+
+    @Override
+    public <T extends CatalystSerializable> void heartbeat(long xid, RemoteObj ro, T logged) {
+        getConnection().send(new HeartbeatRequest<>(ro, xid, logged));
+    }
+
+    @Override
+    public void prepare(long xid)
+    {
+        getConnection().send(new PrepareRequest(xid));
+    }
+
+    @Override
+    public void prepared(long xid, RemoteObj ro) {
+        getConnection().send(new PreparedRequest(xid, ro));
+    }
+
+
+    @Override
+    public void abort(long xid) {
+        getConnection().send(new AbortRequest(xid));
     }
 }
