bf3e847464e200abf4320f3ebf58cd7525b18b57
Seriyin <redan.blue27@gmail.com>
2018-01-04 16:40:28 +0000

    Purchase request can be created from cart or store. Must identify client.

--- bookstore/src/messaging/bank/PurchaseRequest.java
+++ bookstore/src/messaging/bank/PurchaseRequest.java
@@ -1,31 +1,44 @@
-package messaging;
+package messaging.bank;
 
 import bank.Item;
 import bank.Payment;
 import io.atomix.catalyst.buffer.BufferInput;
 import io.atomix.catalyst.buffer.BufferOutput;
 import io.atomix.catalyst.serializer.Serializer;
+import messaging.util.ObjRequest;
+import store.Book;
 import util.RemoteObj;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Purchase Request takes the payment to synchronize in the bank.
+ * Purchase Request takes the list of books from the cart and
+ * client id from the store to synchronize in the bank.
  *
  * Constructs the payment from the cart list.
  */
-public class PurchaseRequest extends ObjRequest {
+public class PurchaseRequest extends ObjRequest
+{
     private Payment p;
     private long cid;
 
-    public PurchaseRequest(RemoteObj ro, List<Book> bl) {
+    public PurchaseRequest(RemoteObj ro, long cid, List<store.Item<Book>> bl) {
         super(ro);
         this.p = new Payment(bl.stream()
                                .collect(ArrayList::new,
-                                       (li,b) -> li.add(new Item(b.getTitle(),b.getPrice())),
-                                       (li1,li2) -> li1.addAll(li2)));
+                                       (li,b) -> li.add(new Item(b.getItem().getTitle(),
+                                                                 b.getItem().getPrice(),
+                                                                 b.getQuantity())),
+                                        ArrayList::addAll));
         this.cid = cid;
+    }
+
+    public PurchaseRequest(RemoteObj ro, long cid, Payment p)
+    {
+        super(ro);
+        this.cid = cid;
+        this.p = p;
     }
 
     public Payment getPayment() {
@@ -39,7 +52,7 @@ public class PurchaseRequest extends ObjRequest {
     @Override
     public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
         super.writeObject(buffer, serializer);
-        serializer.writeObject(p);
+        serializer.writeObject(p, buffer);
         buffer.writeLong(cid);
     }
 
