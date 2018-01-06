89887c88341cac63d096b62cd4f34003273b0f03
Seriyin <redan.blue27@gmail.com>
2018-01-06 01:38:19 +0000

    Added empty CartBuyRequest.

---
+++ bookstore/src/pt/um/bookstore/messaging/store/CartBuyRequest.java
@@ -0,0 +1,11 @@
+package pt.um.bookstore.messaging.store;
+
+/**
+ * TODO cart buy separate from store buy.
+ */
+public class CartBuyRequest
+{
+    public CartBuyRequest(long cid)
+    {
+    }
+}
