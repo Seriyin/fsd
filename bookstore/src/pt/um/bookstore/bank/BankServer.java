86efd6832b3e90a9babf1071696a05a5e1ae03b6
Seriyin <redan.blue27@gmail.com>
2018-01-06 01:30:28 +0000

    Register handlers and await quit from stdin.

--- bookstore/src/pt/um/bookstore/bank/BankServer.java
+++ bookstore/src/pt/um/bookstore/bank/BankServer.java
@@ -1,25 +1,26 @@
-package bank;
+package pt.um.bookstore.bank;
 
 
-import io.atomix.catalyst.concurrent.ThreadContext;
 import io.atomix.catalyst.serializer.Serializer;
 import io.atomix.catalyst.transport.Connection;
 import io.atomix.catalyst.transport.Transport;
-import messaging.bank.*;
-import messaging.store.BuyReply;
-import messaging.store.BuyRequest;
-import util.Server;
+import pt.um.bookstore.handlers.bank.ClientRequestHandler;
+import pt.um.bookstore.handlers.bank.ConsultRequestHandler;
+import pt.um.bookstore.handlers.bank.PurchaseRequestHandler;
+import pt.um.bookstore.messaging.bank.*;
+import pt.um.bookstore.util.Server;
 
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * BankServer class contains the main logic to boot up a process in a connected
  * network.
  * @see pt.haslab.ekit.Clique
  */
-public class BankServer extends Server {
+public class BankServer extends Server
+{
     private String name;
     private Bank b;
 
@@ -30,7 +31,7 @@ public class BankServer extends Server {
      * <p>
      * Should follow bank_{name} convention but is
      * not required.
-     * @param name The name of the bank to registerPayment
+     * @param name The name of the bank to register
      */
     public BankServer(String name)
     {
@@ -69,10 +70,13 @@ public class BankServer extends Server {
     @Override
     protected void execute()
     {
-        ThreadContext tc = getThreadContext();
-        tc.execute(() -> {
-
-        });
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            while(!bf.readLine().equals("quit"));
+        }
+        catch (IOException e) {
+            //ups
+        }
     }
 
     /**
@@ -89,8 +93,6 @@ public class BankServer extends Server {
         Serializer sr = getSerializer();
         sr.register(PurchaseRequest.class);
         sr.register(PurchaseReply.class);
-        sr.register(BuyRequest.class);
-        sr.register(BuyReply.class);
         sr.register(ClientRequest.class);
         sr.register(ClientReply.class);
         sr.register(ConsultRequest.class);
@@ -106,51 +108,9 @@ public class BankServer extends Server {
     @Override
     protected void handlers(Connection c)
     {
-        c.handler(PurchaseRequest.class, new PurchaseRequestHandler());
-        c.handler(ConsultRequest.class, new ConsultRequestHandler());
-        c.handler(ClientRequest.class, new ClientRequestHandler());
-    }
-
-    /**
-     * TODO including 2PC steps for registration.
-     */
-    private class PurchaseRequestHandler
-            implements Function<PurchaseRequest, CompletableFuture<PurchaseReply>>
-    {
-
-        @Override
-        public CompletableFuture<PurchaseReply> apply(PurchaseRequest rq)
-        {
-            b.registerPayment(rq.getClientID(), rq.getPayment());
-            return CompletableFuture.completedFuture(new PurchaseReply(true));
-        }
-    }
-
-    /**
-     * ConsultRequest returns the payment list for the client requested.
-     */
-    private class ConsultRequestHandler
-            implements Function<ConsultRequest,CompletableFuture<ConsultReply>>
-    {
-        @Override
-        public CompletableFuture<ConsultReply> apply(ConsultRequest rq) {
-            List<Payment> lp = b.consult(rq.getClientID());
-            return CompletableFuture.completedFuture(new ConsultReply(lp));
-        }
-    }
-
-    /**
-     * ClientRequest registers a new client into the bank.
-     */
-    private class ClientRequestHandler
-            implements Function<ClientRequest, CompletableFuture<ClientReply>>
-    {
-        @Override
-        public CompletableFuture<ClientReply> apply(ClientRequest rq)
-        {
-            b.registerClient(rq.getName());
-            return CompletableFuture.completedFuture(new ClientReply(true));
-        }
+        c.handler(PurchaseRequest.class, new PurchaseRequestHandler(b));
+        c.handler(ConsultRequest.class, new ConsultRequestHandler(b));
+        c.handler(ClientRequest.class, new ClientRequestHandler(b));
     }
 
 
